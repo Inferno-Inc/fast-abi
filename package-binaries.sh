2593da0098e3cba4c89676439d01f1802d157132
Will Pote <will@uniswap.org>
2021-07-06 14:36:07 -0400

    Initial fork commit: Use ethers BN, change build process, change int32 handling

---
+++ package-binaries.sh
@@ -0,0 +1,6 @@
+npx node-pre-gyp package --target_arch=x64 --target_platform=darwin --target=12.0.0
+npx node-pre-gyp package --target_arch=x64 --target_platform=linux --target=12.0.0
+npx node-pre-gyp package --target_arch=x64 --target_platform=darwin --target=14.0.0
+npx node-pre-gyp package --target_arch=x64 --target_platform=linux --target=14.0.0
+npx node-pre-gyp package --target_arch=x64 --target_platform=darwin --target=16.0.0
+npx node-pre-gyp package --target_arch=x64 --target_platform=linux --target=16.0.0
